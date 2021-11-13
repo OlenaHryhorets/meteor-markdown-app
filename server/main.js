102d7cfa1a149ff23abf2dc611b8f19a784d6c8b
Olena Hryhorets <olena.h@scopicsoftware.com>
2021-11-13 16:04:44 +0200

    created bins publication

--- server/main.js
+++ server/main.js
@@ -1,7 +1,10 @@
 import { Meteor } from 'meteor/meteor';
-
 import { Bins } from '../imports/api/bins';
 
 Meteor.startup(() => {
+  Meteor.publish('bins', function() {
+    // publish only bins that are owned by the current user
+    return Bins.find({ ownerId: this.userId });
+  });
   
 });
