818a9616860dd20b41a3b30cb75c0047e0b721f0
Olena Hryhorets <olena.h@scopicsoftware.com>
2021-11-13 20:04:09 +0200

    displaying bins list

--- client/components/bins/BinsList.jsx
+++ client/components/bins/BinsList.jsx
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import { withTracker } from "meteor/react-meteor-data";
+import { withTracker } from 'meteor/react-meteor-data';
 import { Bins } from '../../../imports/api/bins';
 
 class BinsList extends Component {
+
+    renderList() {
+      return this.props.bins.map( bin => {
+          return (
+                <li className="list-group-item" key={bin._id}>
+                    Bin {bin._id}
+                </li>
+            );
+        })   
+    }
+
     render() {
         console.log(this.props.bins);
         return (
             <div>
                 Bins List
+                <ul className="list-group">
+                    {this.renderList()}
+                </ul>
             </div>
         )
     }
