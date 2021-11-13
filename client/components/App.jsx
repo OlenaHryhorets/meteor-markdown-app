318629656cb9596d13aad697e30d0ca1d2245c95
Olena Hryhorets <olena.h@scopicsoftware.com>
2021-11-13 16:18:01 +0200

    created subscription to consume data from publication

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import BinsList from './bins/BinsList';
 import Header from './Header'
 
 export const App = () => (
   <div>
     <Header />
+    <BinsList />
   </div>
 );
---
+++ client/components/bins/BinsList.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { withTracker } from "meteor/react-meteor-data";
+import { Bins } from '../../../imports/api/bins';
+
+class BinsList extends Component {
+    render() {
+        console.log(this.props.bins);
+        return (
+            <div>
+                Bins List
+            </div>
+        )
+    }
+}
+
+export default withTracker(() => {
+  // set up subscription
+  Meteor.subscribe("bins");
+  return { bins: Bins.find({}).fetch() };
+})(BinsList);
